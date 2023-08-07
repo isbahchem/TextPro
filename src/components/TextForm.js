f54f190ce72495ef1cb4c76a51e52ee3d3e739f4
Isbah Ahmed Feroz <64509937+isbahchem@users.noreply.github.com>
2023-08-07 20:22:51 +0500

    Bug Fixes in TextForm + Alert.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -55,7 +55,7 @@ export default function TextForm(props){
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 
-                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'grey':'white', color: props.mode === 'dark'?'white':'black'}} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'#13466e':'white', color: props.mode === 'dark'?'white':'black'}} id="myBox" rows="8"></textarea>
                 
             </div>
             <button className='btn btn-primary mx-1 my-1' onClick={handleUpClick}>Convert to Uppercase</button>
@@ -67,7 +67,7 @@ export default function TextForm(props){
         <div className='container' style={{backgroundColor: props.mode === 'dark'?'white':'black', color: props.mode === 'dark'?'black':'white'}}>
             <h2>Your text summary</h2>
             <p>{text.split(" ").filter((element)=>{return element.length!=0}).length} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <p>{0.008 * text.split(" ").filter((element)=>{return element.length!=0}).length} Minutes read</p>
             <h2>Preview</h2>
             <p>{text.length > 0 ? text : "Enter something to preview it here."}</p>
         </div>
